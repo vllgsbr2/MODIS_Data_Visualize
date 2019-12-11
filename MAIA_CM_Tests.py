eef1d95113b6964450a26af404d00563cf8637fc
Javier Villegas <vllgsbr2@illinois.edu>
2019-12-10 20:20:56 -0600

    Delete MAIA_CM_Tests.py

--- MAIA_CM_Tests.py
+++
@@ -1,9 +0,0 @@
-'''
-Here we create the relevant MODIS tests plus the additional MAIA tests to
-reproduce MODIS cloud mask results. The reference data is the MODIS cloud mask
-results of cloudiness and 5 comparable tests (comparable in wavelength). This
-exercise is to form a base line for the MAIA cloud mask algorithm.
-'''
-import numpy as np
-import h5py
-import time
